3831fd1a0aae50d883b2cfb740e0284407609c4c
bhanunallabati <78374021+bhanunallabati@users.noreply.github.com>
2025-03-26 10:02:06 -0400

    Update callsutils.py

--- callsutils.py
+++ callsutils.py
@@ -5,42 +5,51 @@ from airflow_collibra_dq_provider.operators.dq import (
     CollibraDQJobOperator
 )
 
-def create_aggregate_score_task(task_id, config, dag):
+class Collibra:
     """
-    Create a Collibra Aggregate Score Task.
+    A utility class for creating Collibra-related Airflow tasks.
     """
-    return CollibraDQCHeckAggregateScoreOperator(
-        task_id=task_id,
-        config=config,
-        dag=dag
-    )
 
-def create_rule_task(task_id, rule_config, dag):
-    """
-    Create a Collibra Rule Check Task.
-    """
-    return CollibraDQCHeckRuleOperator(
-        task_id=task_id,
-        rule_config=rule_config,
-        dag=dag
-    )
+    @staticmethod
+    def create_aggregate_score_task(task_id, config, dag):
+        """
+        Create a Collibra Aggregate Score Task.
+        """
+        return CollibraDQCHeckAggregateScoreOperator(
+            task_id=task_id,
+            config=config,
+            dag=dag
+        )
 
-def create_score_task(task_id, score_config, dag):
-    """
-    Create a Collibra Score Check Task.
-    """
-    return CollibraDQCHeckScoreOperator(
-        task_id=task_id,
-        score_config=score_config,
-        dag=dag
-    )
+    @staticmethod
+    def create_rule_task(task_id, rule_config, dag):
+        """
+        Create a Collibra Rule Check Task.
+        """
+        return CollibraDQCHeckRuleOperator(
+            task_id=task_id,
+            rule_config=rule_config,
+            dag=dag
+        )
 
-def create_job_task(task_id, job_config, dag):
-    """
-    Create a Collibra Job Task.
-    """
-    return CollibraDQJobOperator(
-        task_id=task_id,
-        job_config=job_config,
-        dag=dag
-    )
+    @staticmethod
+    def create_score_task(task_id, score_config, dag):
+        """
+        Create a Collibra Score Check Task.
+        """
+        return CollibraDQCHeckScoreOperator(
+            task_id=task_id,
+            score_config=score_config,
+            dag=dag
+        )
+
+    @staticmethod
+    def create_job_task(task_id, job_config, dag):
+        """
+        Create a Collibra Job Task.
+        """
+        return CollibraDQJobOperator(
+            task_id=task_id,
+            job_config=job_config,
+            dag=dag
+        )
